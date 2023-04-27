c70f11163b856e296d87faf2d5750418afdb3c77
Gamingdy <christgik12@gmail.com>
2023-04-28 01:31:46 +0200

    fix: remove  font-ubuntu from font list because i'm on debian

--- install.sh
+++ install.sh
@@ -50,6 +50,7 @@ sudo apt install -y nitrogen
  
 # Fonts and icons for now
 sudo apt install -y fonts-firacode fonts-liberation2 fonts-ubuntu papirus-icon-theme fonts-cascadia-code
+sudo apt install -y fonts-firacode fonts-liberation2 papirus-icon-theme fonts-cascadia-code
 
 # Openbox packages
 sudo apt install -y openbox dunst dbus-x11 hsetroot i3lock libnotify-bin lximage-qt menu picom  qt5-style-plugins rofi scrot tint2 xfce4-power-manager 
