0020bb785621857f4763f79be898a0ed399dbb9e
Gamingdy <christgik12@gmail.com>
2023-08-20 20:49:36 +0200

    feat: add sublime text installation

--- install.sh
+++ install.sh
@@ -126,6 +126,23 @@ make
 sudo make install
 cd ../../
 
+
+printf "\e[1;32mInstalling Sublime Text.\e[0m\n"
+# Ensure that apt can handle HTTPS sources:
+sudo apt install apt-transport-https gpg -y
+
+# Add Sublime Text repository signing key to verify downloaded packages:
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+
+# Stable releases of Sublime Text:
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# update apt list for sublime text and installing sublime text
+sudo apt update
+
+sudo apt install sublime-text -y
+
+
 # Rofi font
 mkdir -p  ~/.local/share/fonts
 cp -rvf .config/rofi/fonts/* ~/.local/share/fonts
