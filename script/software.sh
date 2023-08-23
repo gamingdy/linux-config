080f702fa4f7c6d43c4ec7cad1107199316dde4c
Gamingdy <christgik12@gmail.com>
2023-08-23 16:02:58 +0200

    feat: add docker installation

--- script/software.sh
+++ script/software.sh
@@ -48,3 +48,15 @@ wget --show-progress -O  termius.deb https://autoupdate.termius.com/linux/Termiu
 sudo apt install -y ./termius.deb
 sudo rm ./termius.deb
 
+# Docker installation
+printf "\e[1;32mInstalling Docker.\e[0m\n"
+sudo apt install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
